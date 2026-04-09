5e4b5da770ce518486ed298472ef17f25dfcf97b
ERICAAAA27 <157084294+ERICAAAA27@users.noreply.github.com>
2026-04-09 15:46:49 +0800

    Describe your Changes here

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-$databaseURL= "https://crnp-31b28-default-rtdb.asia-southeast1.firebasedatabase.app/"; //islis d mikayyy sa firbase mo na ma kwa  hello
+$databaseURL= "https://crnp-31b28-default-rtdb.asia-southeast1.firebasedatabase.app/"; //isli d mikayyy sa firbase mo na ma kwa try lng abi HAHAHAHAHAH
 ?>
