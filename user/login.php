f24338e1d1a1cd600bca944f6cbfd10a0bd8ce82
Erica Cornel Atis <jayralphbonitillo09@gmail.com>
2026-05-18 13:58:59 +0800

    Wala pa ga connect sa database and google auth

--- user/login.php
+++ user/login.php
@@ -4,12 +4,20 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>User Login | Crates N' Plates</title>
-
+    <script src="https://accounts.google.com/gsi/client" async defer></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <link rel="stylesheet" href="css/index-s.css">
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="../styles.css">
 </head>
 
+
+
 <body class="user-auth-body">
+    <div id="g_id_onload"
+     data-client_id="1029428356414-ituvg05tojmq3sva030sab3cdog78fra.apps.googleusercontent.com"
+     data-callback="handleCredentialResponse">
+</div>
 
 <div class="user-split-container">
 
@@ -33,9 +41,13 @@
                 </div>
 
                 <button type="submit" class="user-btn-auth">Login</button>
+                     <div style="display: flex; justify-content: center; margin-bottom: 20px;">
+            <div class="g_id_signin" data-type="standard"></div>
+        </div>
 
             </form>
 
+            <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>  
             <p class="user-auth-footer">
                 Don't have an account? <a href="sign_up.php">Sign Up</a>
             </p>
@@ -51,6 +63,26 @@
     </div>
 
 </div>
+
+
+<script>
+function handleCredentialResponse(response) {
+
+    const form = document.createElement("form");
+    form.method = "POST";
+    form.action = "google-auth.php"; // adjust path
+
+    const input = document.createElement("input");
+    input.type = "hidden";
+    input.name = "token";
+    input.value = response.credential;
+
+    form.appendChild(input);
+    document.body.appendChild(form);
+    form.submit();
+}
+</script>
+
 
 </body>
 </html>
