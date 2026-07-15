351dd37e91d0df761b11d0cdb15464d12854d50f
erica <erco.atis.ui@phinmaed.com>
2026-07-15 13:03:16 +0800

    gitignore: *.log; rm setup.sh start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cd "$(dirname "$0")"
-if ! command -v php &>/dev/null; then
-  for p in "../../php/php.exe" /c/xampp/php/php.exe C:/xampp/php/php.exe; do
-    [ -f "$p" ] && export PATH="$PATH:$(dirname "$p")" && break
-  done
-fi
-php -S localhost:8000
